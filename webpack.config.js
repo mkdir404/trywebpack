431a80ba7000a1cdeb094d5ef22af389dabe2295
Ricardo Alba <ralba@radarappsecurity.com>
2022-02-08 20:08:03 -0600

    :bug: fix

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
                         mimetype : 'application/font-woff',
                         name : '[name].[contenthash].[ext]',
                         outputPath : "./assets/fonts",
-                        publicPath : "../assets/fonts",
+                        publicPath : "./assets/fonts",
                         esModule : false
                     }
                 }
